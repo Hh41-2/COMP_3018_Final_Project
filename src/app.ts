54730b57248c92d0efef772c17dba80aa3c3c265
Heesoo Hwang <email.academic>
2026-04-04 16:36:03 -0500

    feat: added swagger configs #2

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import healthRoute from "./api/v1/routes/healthRoute";
 import hofRoute from "./api/v1/routes/hofRoute";
 import playerRoute from "./api/v1/routes/playerRoute";
 import teamRoute from "./api/v1/routes/teamRoute";
+import setupSwagger from "./config/swagger";
 
 // Initialize Express application
 const app: Express = express();
@@ -16,5 +17,8 @@ app.use("/api/v1", healthRoute);
 app.use("/api/v1", hofRoute);
 app.use("/api/v1", playerRoute);
 app.use("/api/v1", teamRoute);
+
+// Setup Swagger
+setupSwagger(app);
 
 export default app;
