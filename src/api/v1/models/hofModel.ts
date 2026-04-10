611f5276f034d19ec54602c4f1e980b8267b4073
Heesoo Hwang <email.academic>
2026-04-10 00:40:52 -0500

    feat: added swagger schemas for hof, players and teams #3

--- src/api/v1/models/hofModel.ts
+++ src/api/v1/models/hofModel.ts
@@ -1,3 +1,35 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     hof:
+ *       type: object
+ *       required:
+ *         - inductionYear
+ *         - achievements
+ *         - description
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for the hall of fame
+ *         playerId:
+ *           type: string
+ *           description: Player's last name + their date of birth
+ *           example: "lastnameYYYYMMDD"
+ *         inductionYear:
+ *           type: number
+ *           description: The year the player was inducted
+ *         achievements:
+ *           type: array
+ *           minItems: 3 
+ *           description: List of major achievements
+ *           items:
+ *             type: string
+ *         description:
+ *           type: string
+ *           description: A detailed description of the player's legacy and accomplishments
+ */
+
 export interface HallOfFame {
        id: string, // Unique identifier for a hall of fame entry
        playerId: string, // The unique identifier of the inducted player
--- src/api/v1/models/playerModel.ts
+++ src/api/v1/models/playerModel.ts
@@ -1,8 +1,64 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     player:
+ *       type: object
+ *       required:
+ *         - name
+ *         - position
+ *         - dateOfBirth
+ *         - country
+ *         - era
+ *         - teams
+ *         - goals
+ *         - appearances
+ *         - assists
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Unique identifier for the player
+ *         name:
+ *           type: string
+ *           description: The name of the player
+ *         position:
+ *           type: string
+ *           description: The position played by the player
+ *           enum: ["Goalkeeper", "Defender", "Midfielder","Striker"]
+ *           example: "Midfielder"
+ *         dateOfBirth:
+ *           type: string
+ *           description: The date of birth of the player
+ *           example: "YYYYMMDD"
+ *         country:
+ *           type: string
+ *           description: The country the player is from
+ *         era:
+ *           type: string
+ *           description: The era/period when the player was active
+ *           example: "1999-2020"
+ *         teams:
+ *           type: array
+ *           minItems: 1 
+ *           description: List of teams a player has played for
+ *           items:
+ *             type: string
+ *         goals:
+ *           type: number 
+ *           description: Total number of goals scored by the player
+ *         appearances:
+ *           type: number
+ *           description: Total number of appearances made by the player 
+ *         assists:
+ *           type: number
+ *           description: The total number of assists by the player
+ */
+
 export interface Player {
        id: string, // Unique Identifier for a player
        name: string, // The name of the player
        position: string, // The position played by the player
-       dateOfBirth: string, //  The date of birth of the player
+       dateOfBirth: string, // The date of birth of the player
        country: string, // The country the player is from
        era: string, // The era/period when the player was active
        teams: string[], // List of teams a player has played for
