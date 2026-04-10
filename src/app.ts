dd39af360b66043b8158661eead7424c1c831112
Heesoo Hwang <email.academic>
2026-04-10 00:34:39 -0500

    feat: installed dependencies #3

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express, { Express } from "express";
+import dotenv from "dotenv";
+
+// Load environment variables BEFORE your internal imports!
+dotenv.config();
+
 import healthRoute from "./api/v1/routes/healthRoute";
 import hofRoute from "./api/v1/routes/hofRoute";
 import playerRoute from "./api/v1/routes/playerRoute";
 import teamRoute from "./api/v1/routes/teamRoute";
 import setupSwagger from "./config/swagger";
 
+
 // Initialize Express application
 const app: Express = express();
+
+// Body parsing middleware
+app.use(express.json());
 
 // Define a route
 app.get("/", (req, res) => {
