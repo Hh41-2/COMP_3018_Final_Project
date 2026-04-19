07fca469ba597cd2daaafda1fc9b5e8cd2f6954d
Heesoo Hwang <email.academic>
2026-04-18 21:02:42 -0500

    feat: modified team service, controller and route layers #4

--- src/api/v1/controllers/teamController.ts
+++ src/api/v1/controllers/teamController.ts
@@ -1,29 +1,108 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { HTTP_STATUS } from "../../../constants/httpConstant";
 import * as teamService from "../services/teamService";
+import { Team } from "../models/teamModel"
+import { successResponse } from "../models/responseModel";
 
 
-export const getAllTeam = (req: Request, res: Response) => {
-       res.status(HTTP_STATUS.OK).json({message: teamService.getAllTeam()}
-       );
+/**
+ * Handles retrieving all teams.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const getAllTeam = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+       try{
+              const getAllTeams: Team[] = await teamService.getAllTeam();
+              res.status(HTTP_STATUS.OK).json({
+                     message: "Retrieved all teams",
+                     totalPlayers: getAllTeams.length,
+                     data: getAllTeams 
+              });
+       } catch (error: unknown) {
+              next(error);
+       }
 };
 
-export const getTeam = (req: Request, res: Response) => {
-       res.status(HTTP_STATUS.OK).json({}
-       );
+/**
+ * Handles retrieving a team.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const getTeam = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+       try{
+              const getTeamById: Team | null = await teamService.getTeam(req.params.id as string);
+              if(getTeam === null){
+                     res.status(HTTP_STATUS.BAD_REQUEST).json({message:"Player not found"});
+              } 
+
+              res.status(HTTP_STATUS.OK).json(successResponse(
+                     `Retrieved team with id: ${req.params.id}`,
+                     getTeamById
+              ));
+       } catch (error: unknown) {
+              next(error);
+       }
 };
 
-export const createTeam = (req: Request, res: Response) => {
-       res.status(HTTP_STATUS.OK).json({message: teamService.createTeam()}
-       );
+/**
+ * Handles creating a new team.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const createTeam = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+       try{
+              const createTeam: Team = await teamService.createTeam(req.body);
+
+              res.status(HTTP_STATUS.OK).json(successResponse(
+                     "Team created",
+                     createTeam
+              ));
+       } catch (error: unknown) {
+              next(error);
+       }
 };
 
-export const updateTeam = (req: Request, res: Response) => {
-       res.status(HTTP_STATUS.OK).json({message: teamService.updateTeam()}
-       );
+/**
+ * Handles updating a team.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const updateTeam = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+       try{
+              const updateTeam: Team = await teamService.updateTeam(req.params.id as string, req.body);
+
+              res.status(HTTP_STATUS.OK).json(successResponse(
+                     "Team updated",
+                     updateTeam
+              ));
+       } catch (error: unknown) {
+              next(error);
+       }
 };
 
-export const deleteTeam = (req: Request, res: Response) => {
-       res.status(HTTP_STATUS.OK).json({message: teamService.deleteTeam()}
-       );
+/**
+ * Handles deleting a team.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ * @returns {Promise<void>}
+ */
+export const deleteTeam = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+       try{
+              const deleteTeam: string = await teamService.deleteTeam(req.params.id as string);
+
+              res.status(HTTP_STATUS.OK).json(successResponse(
+                     `Delete team with id: ${deleteTeam}`
+              ));
+       } catch (error: unknown) {
+              next(error);
+       }
 };
--- src/api/v1/services/teamService.ts
+++ src/api/v1/services/teamService.ts
@@ -1,46 +1,61 @@
-// import { Team } from "../models/teamModel";
-// import * as firestoreRepository from "../repositories/firestoreRepository";
+import { Team } from "../models/teamModel";
+import * as firestoreRepository from "../repositories/firestoreRepository";
+
+const TEAM_COLLECTION = "teams";
 
 /**
  * Retrieves all the teams in the database
  * @returns returns a list of team
  */
-export const getAllTeam = (): string => {
-       return "getAllTeam";
+export const getAllTeam = async (): Promise<Team[]> => {
+       const allTeam = await firestoreRepository.getDocuments(TEAM_COLLECTION);
+       
+       return allTeam.docs.map(doc => doc.data() as Team);
 } 
 
 /**
  * Retrieves the team in the database by id
  * @param id - Unique identifier for the team
- * @returns returns the team with a specified id
+ * @returns returns the team with a specified id or null if a team is not found
  */
-export const getTeam = async () => {
-       return "getTeam";
+export const getTeam = async (id: string): Promise<Team | null> => {
+       const getTeamById = await firestoreRepository.getDocumentById(TEAM_COLLECTION, id);
+       
+       return getTeamById ? getTeamById.data() as Team : null;
 } 
+
 
 /**
  * Creates the team in the database
- * @param 
+ * @param team: Partial<Team> - Required fields without id for team
  * @returns returns the created team
  */
-export const createTeam = (): string => {
-       return "testing createTeam";
+export const createTeam = async (team: Partial<Team>): Promise<Team> => {
+       const createTeam = await firestoreRepository.createDocument(TEAM_COLLECTION, team);
+
+       return createTeam;
 } 
 
 /**
  * Updatess the team in the database by id
  * @param id - Unique identifier for the team
+ * @param team: Partial<Team> - Required fields without id for team
  * @returns returns the updated team
  */
-export const updateTeam = async () => {
-       return "updateTeam";
+export const updateTeam = async (id: string, team: Partial<Team>): Promise<Team> => {
+       const updateTeam = await firestoreRepository.updateDocument(TEAM_COLLECTION, id, team);
+
+       return updateTeam;
 } 
 
 /**
  * Deletes the team in the database by id
  * @param id - Unique identifier for team
+ * @returns returns the deleted team's id
  * 
  */
-export const deleteTeam = async () => {
-       return "deleteTeam";
+export const deleteTeam = async (id: string): Promise<string> => {
+       const deleteTeam: string = await firestoreRepository.deleteDocument(TEAM_COLLECTION, id);
+
+       return deleteTeam;
 } 
